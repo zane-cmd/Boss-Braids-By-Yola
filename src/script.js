2d171aeeda7a8c1e29325e766f7ae8fe8a582220
zane-cmd <zanevanrooyenpg@gmail.com>
2024-07-27 09:56:02 +0000

    gstyle complete

--- src/script.js
+++ src/script.js
@@ -1,31 +1,33 @@
-const swiper = new Swiper('.slider-wrapper', {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 30,
+document.addEventListener('DOMContentLoaded', (event) => {
+    const num1 = document.querySelector('.num1');
+    const num2 = document.querySelector('.num2');
 
-  // Pagination bullets
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true
-  },
+    num2.addEventListener('click', () => {
+      num2.classList.add('p-active');
+      num1.classList.remove('p-active');
+    });
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    num1.addEventListener('click', () => {
+      num1.classList.add('p-active');
+      num2.classList.remove('p-active');
+    });
+  });
 
-  // Responsive breakpoints
-  breakpoints: {
-    0: {
-      slidesPerView: 1
-    },
-    710: {
-      slidesPerView: 2
-    },
-    1024: {
-      slidesPerView: 3
+
+  //gstyle page
+
+  var mainImage = document.getElementById('MainImg');
+  var smallimg = document.getElementsByClassName('small-img');
+  
+  function updateMainImage(index) {
+    var imgSrc = smallimg[index].src;
+    mainImage.style.backgroundImage = `url(${imgSrc})`;
+    mainImage.style.backgroundSize = 'cover'; // Adjust as needed
+    
+  }
+  
+  for (let i = 0; i < smallimg.length; i++) {
+    smallimg[i].onclick = function() {
+      updateMainImage(i);
     }
   }
-});
